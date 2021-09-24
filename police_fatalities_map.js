04fad841bef1bb32a5d45d7009a8350e9d96c7ac
GeorgeTan615 <87482146+GeorgeTan615@users.noreply.github.com>
2021-09-24 13:35:57 +0800

    Fixed naming of choropleth map

--- police_fatalities_map.js
+++ police_fatalities_map.js
@@ -1,4 +1,4 @@
 var vg_1 = "police_fatalities.json";
-vegaEmbed("#chloropleth_map", vg_1).then(function(result) {
+vegaEmbed("#choropleth_map", vg_1).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
